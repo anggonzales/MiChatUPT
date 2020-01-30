43ec9453d25db857374f41913f16ff4b92a739f0
anggonzales <anggonzalesca@upt.pe>
2020-01-30 11:01:30 -0500

    first commit

---
+++ app/src/main/java/com/example/michatupt/App.java
@@ -0,0 +1,26 @@
+package com.example.michatupt;
+
+
+import android.app.Application;
+
+import io.socket.client.IO;
+import io.socket.client.Socket;
+
+public class App extends Application {
+    private static Socket mSocket;
+    @Override
+    public void onCreate() {
+        super.onCreate();
+//Se crea un socket que apunta a la ruta que se muestra
+        try {
+            //mSocket = IO.socket("http://angelmoviles.eu-4.evennode.com/");
+            mSocket = IO.socket("http://wiluptchat.eu-4.evennode.com/");
+        }
+        catch (Exception e){
+        }
+    }
+    public static Socket getSocket() {
+        return mSocket;
+    }
+}
+
